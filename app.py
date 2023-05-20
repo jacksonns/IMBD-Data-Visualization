07a414d30b619ff20c86f44c20db00b44c4d32cb
jacksonns <jackson.guima2@gmail.com>
2023-05-20 16:38:30 -0300

    Visualizações de filmes por ano e gênero

--- app.py
+++ app.py
@@ -2,43 +2,64 @@ from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 import dash_bootstrap_components as dbc
+import numpy as np
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
 # Initialize the app and incorporate a Dash Bootstrap theme
 external_stylesheets = [dbc.themes.COSMO]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 
+movies = pd.read_csv('data/title.basics.tsv', sep='\t')
+movies = movies.query(" titleType == 'movie' ")
+nan_index = movies[movies['startYear'] == '\\N'].index
+movies_with_year = movies.drop(nan_index)
+year_counts = movies_with_year['startYear'].value_counts().sort_index()
+
+def movies_per_year_graph():
+    fig = px.line(year_counts, title='Quantidade de filmes lançados por ano')
+    fig.update_layout(
+        xaxis_title="Ano",
+        yaxis_title="Número de Filmes",
+        showlegend=False
+    )
+    return fig
+
+
+nan_genres = movies[movies['genres'] == '\\N'].index
+movies_with_genres = movies.drop(nan_genres)
+movies_genres_column = movies_with_genres['genres'].str.split(',')
+genres_expanded = movies_genres_column.explode().value_counts()
+
+def genre_distribution_graph():
+    fig = px.bar(genres_expanded, title='Distribuição de filmes por gênero')
+    fig.update_layout(
+        xaxis_title=None,
+        yaxis_title=None,
+        showlegend=False
+    )
+    return fig
+
+
 app.layout = dbc.Container([
     dbc.Row([
-        html.Div('My First App with Data, Graph, and Controls', className="text-primary text-center fs-3")
+        html.Div('Visualização de Dados IMDB', className="text-primary text-center fs-3")
     ]), 
 
     dbc.Row([
         dbc.Col([
-            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
+            dcc.Graph(id='movies_per_year', figure=movies_per_year_graph()), 
         ], width=6),
 
         dbc.Col([
-            dbc.RadioItems(options=[{"label": x, "value": x} for x in ['pop', 'lifeExp', 'gdpPercap']],
-                       value='lifeExp',
-                       inline=True,
-                       id='radio-buttons-final'),
-            dcc.Graph(figure={}, id='my-first-graph-final')
+            dcc.Graph(id='genre_distribution', figure=genre_distribution_graph()), 
         ], width=6),
     ]),
 
 ], fluid=True)
 
-@callback(
-    Output(component_id='my-first-graph-final', component_property='figure'),
-    Input(component_id='radio-buttons-final', component_property='value')
-)
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
-    return fig
+
+
 
 # Run the app
 if __name__ == '__main__':
